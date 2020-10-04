99c2d7ae5c2855552b8a03ee9a1330572e1f3ea7
Łukasz Rutkowski <lukus178@gmail.com>
2020-10-04 12:46:05 +0200

    Prepare new design

---
+++ Shared/Apps/Food Menu/FoodMenuApp.swift
@@ -0,0 +1,34 @@
+//
+//  FoodMenuApp.swift
+//  PixelPerfect (iOS)
+//
+//  Created by Łukasz Rutkowski on 04/10/2020.
+//
+
+import SwiftUI
+
+extension AppInfo {
+
+    static let foodMenuApp = AppInfo(
+        id: "foodmenu",
+        name: "Food Menu",
+        author: "Unknown",
+        url: "https://www.pinterest.com/pin/774124922767471/",
+        accentColor: #colorLiteral(red: 0.9607843137, green: 0.231372549, blue: 0.3137254902, alpha: 1)
+    )
+}
+
+struct FoodMenuApp: View {
+    var body: some View {
+        VStack {
+            Text("Pizza")
+            Text("25 items")
+        }
+    }
+}
+
+struct FoodMenuApp_Previews: PreviewProvider {
+    static var previews: some View {
+        FoodMenuApp()
+    }
+}
