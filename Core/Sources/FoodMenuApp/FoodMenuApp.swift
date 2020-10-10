acc082be7df8e138d35e86bfa869394a6154ae3c
Łukasz Rutkowski <lukus178@gmail.com>
2020-10-10 20:29:55 +0200

    [FoodMenu] Add navigation bar and animations

--- Core/Sources/FoodMenuApp/FoodMenuApp.swift
+++ Core/Sources/FoodMenuApp/FoodMenuApp.swift
@@ -25,38 +25,84 @@ public struct FoodMenuApp: View {
     @State private var step = 1
 
     public var body: some View {
-        ZStack(alignment: .bottom) {
+        ZStack {
             PizzaBackground(progress: CGFloat(step) / 3)
 
-            ScrollView {
-                VStack(alignment: .leading, spacing: 32) {
-                    if step > 1 {
-                        Text("Salads menu")
-                            .font(.largeTitleUltra)
-                            .frame(maxWidth: 180)
-                            .foregroundColor(.white)
-                            .padding([.top, .horizontal], 24)
+            GeometryReader { geometry in
+                ScrollView {
+                    ScrollViewReader { proxy in
+                        VStack(alignment: .leading, spacing: 32) {
+                            Spacer(minLength: 12)
+                            if step > 1 {
+                                Text(step == 3 ? "Shopping cart" : "Salads menu")
+                                    .font(.largeTitleUltra)
+                                    .foregroundColor(.white)
+                                    .padding([.top, .horizontal], 24)
+                                    .frame(maxWidth: geometry.size.width * 2/3, alignment: .leading)
+                                    .animation(.none)
+                            }
+                            FoodCategoryView(categoryName: "Pizza", itemCount: 25)
+                            FoodItemView()
+                            FoodCategoryView(categoryName: "Salads", itemCount: 30)
+                            FoodCategoryView(categoryName: "Desserts", itemCount: 30)
+                            FoodCategoryView(categoryName: "Pasta", itemCount: 44)
+                            FoodCategoryView(categoryName: "Beverages", itemCount: 30)
+                        }
                     }
-                    FoodCategoryView(categoryName: "Pizza", itemCount: 25)
-                    FoodItemView()
-                    FoodCategoryView(categoryName: "Salads", itemCount: 30)
-                    FoodCategoryView(categoryName: "Desserts", itemCount: 30)
-                    FoodCategoryView(categoryName: "Pasta", itemCount: 44)
-                    FoodCategoryView(categoryName: "Beverages", itemCount: 30)
                 }
             }
 
-            ShoppingCartView()
-        }
-        .onTapGesture {
-            withAnimation {
-                step += 1
-                if step > 3 {
-                    step = 1
+            HStack {
+                if step > 1 {
+                    Button(action: goBack) {
+                        Image(systemName: "arrow.left")
+                            .imageScale(.large)
+                            .font(Font.body.bold())
+                            .frame(width: 24, height: 24)
+                    }
+                    .accentColor(.white)
                 }
+
+                Spacer()
+
+                Button(action: goToCart) {
+                    Image(systemName: "cart")
+                        .imageScale(.large)
+                        .font(Font.body.bold())
+                        .frame(width: 24, height: 24)
+                }
+                .accentColor(step == 3 ? .white : AppInfo.foodMenuApp.accentColor)
+            }
+            .padding(.horizontal, 32)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+
+            if step == 3 {
+                ShoppingCartView()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+                    .zIndex(1)
+                    .transition(.move(edge: .bottom))
             }
         }
+        .onTapGesture(perform: goForward)
         .ignoresSafeArea(.all, edges: .bottom)
+    }
+
+    private func goToCart() {
+        withAnimation { step = 3 }
+    }
+
+    private func goBack() {
+        guard step > 1 else { return }
+        withAnimation {
+            step -= 1
+        }
+    }
+
+    private func goForward() {
+        guard step < 3 else { return }
+        withAnimation {
+            step += 1
+        }
     }
 }
 
